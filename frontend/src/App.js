8ab9a28701a6236b06c77fc4a5dc91e5a9c08cd7
Atrijo Gangopadhyay <arrijo2001@gmail.com>
2021-11-09 20:49:54 +0530

    register form layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 import Footer from "./components/Footer"
 import Homepage from "./components/Homepage"
 import Navbar from "./components/Navbar"
+import Register from './screens/Register'
 
 //import the states
 import AuthState from "./context/auth/AuthState"
+
+//Dependency imports
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 const App = () => {
   return (
     <div>    
       <AuthState>
-          <Navbar/>
-          <Homepage/>
-          <Footer/>
+      <Navbar/>
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Homepage/>}/>
+              <Route path='/register' element={<Register/>}/>
+          </Routes>
+        </BrowserRouter>
+        <Footer/>
       </AuthState>
     </div>
   )
---
+++ frontend/src/screens/Register.js
@@ -0,0 +1,10 @@
+const Register = () => {
+    return (
+        <div className="container">
+            <h2 className="text-center">Employee Registration</h2>
+            <h2>This is the register page</h2>
+        </div>
+    )
+}
+
+export default Register
